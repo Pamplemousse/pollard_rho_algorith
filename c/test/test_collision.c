6f282f80cfe252ca92ab3f670b1a4d3cb189ea6b
Pamplemousse <xav.maso@gmail.com>
2018-02-12 10:23:57 +0100

    Enlever les espaces inutiles et `debug.o` du Makefile

--- c/test/test_collision.c
+++ c/test/test_collision.c
@@ -36,34 +36,34 @@ void test_Floyd(void) {
   mpz_t beta    ; mpz_init_set_d(beta, 228);
 
   mpz_t expected_ai, expected_bi, expected_Ai, expected_Bi;
-  
+
   mpz_init_set_d(expected_ai, 12);
   mpz_init_set_d(expected_bi, 38);
   mpz_init_set_d(expected_Ai, 10);
   mpz_init_set_d(expected_Bi, 104);
 
   mpz_t result[4];
-  
+
   for (unsigned int i = 0; i < 4; i++) {
     mpz_init(result[i]);
   }
-  
+
   Floyd(&result, alpha, beta, modulus, order);
- 
+
   ASSERT (mpz_cmp(&expected_ai, result[0]) == 0);
   ASSERT (mpz_cmp(&expected_bi, result[1]) == 0);
   ASSERT (mpz_cmp(&expected_Ai, result[2]) == 0);
   ASSERT (mpz_cmp(&expected_Bi, result[3]) == 0);
- 
+
   for (unsigned int i = 0; i < 4; i++) {
     mpz_clear(result[i]);
   }
-  
+
   mpz_clear(expected_ai);
   mpz_clear(expected_bi);
   mpz_clear(expected_Ai);
   mpz_clear(expected_Bi);
-  
+
   mpz_clear(order);
   mpz_clear(modulus);
   mpz_clear(alpha);
@@ -73,7 +73,6 @@ void test_Floyd(void) {
 
 
 int main (void) {
-  
   DO_TEST (test_Floyd);
 
   return EXIT_SUCCESS;
