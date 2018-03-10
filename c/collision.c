374cc2c23a2fb0eb4d26e9d3fcdffb3c51b80d74
Laure <laure.bachelet2@gmail.com>
2018-03-10 12:59:56 +0100

    Détecter une collision dans le cas où a0=0 et b0=0 ne donnent rien

--- c/collision.c
+++ c/collision.c
@@ -17,16 +17,53 @@ void Floyd (mpz_t *result,
   mpz_init_set_d(Ai, 0);
   mpz_init_set_d(Bi, 0);
 
+  mpz_t i;
+  mpz_init_set_d(i, 0);
+
   while (1) {
     f(&xi, &ai, &bi, alpha, beta, modulus, order);
 
     f(&Xi, &Ai, &Bi, alpha, beta, modulus, order);
     f(&Xi, &Ai, &Bi, alpha, beta, modulus, order);
 
+    mpz_add_ui(i, i, 1);
+    if (mpz_cmp(i, *order) == 0)
+      break;
+
     if ((mpz_cmp(xi, Xi) == 0) && (mpz_cmp(bi, Bi) != 0)){
       break;
     }
   }
+
+  mpz_clear(i);
+  if (mpz_cmp(xi, Xi) != 0)
+    {
+      gmp_randstate_t state;
+      gmp_randinit_mt(state);
+      mpz_urandomm(ai, state, *order);
+      mpz_urandomm(bi, state, *order);
+      mpz_t inter_1, inter_2;
+      mpz_init_set_d(inter_1, 1);
+      mpz_init_set_d(inter_2, 1);
+      mpz_powm(inter_1, *alpha, ai, *modulus);
+      mpz_powm(inter_2, *beta, bi, *modulus);
+      mpz_mul(xi, inter_1, inter_2);
+      mpz_set(Xi, xi);
+      mpz_set(Ai, ai);
+      mpz_set(Bi, bi);
+      gmp_randclear(state); mpz_clear(inter_1); mpz_clear(inter_2);
+
+      while (1) {
+	f(&xi, &ai, &bi, alpha, beta, modulus, order);
+
+        f(&Xi, &Ai, &Bi, alpha, beta, modulus, order);
+        f(&Xi, &Ai, &Bi, alpha, beta, modulus, order);
+
+        if ((mpz_cmp(xi, Xi) == 0) && (mpz_cmp(bi, Bi) != 0)){
+          break;
+	}
+      }
+    }
 
   mpz_set(result[0], ai);
   mpz_set(result[1], bi);
